c8e8e7435c8d4d615e726966ce18a369b499ed3b
HarshitaRasekar <155139335+HarshitaRasekar@users.noreply.github.com>
2025-08-09 11:46:19 +0530

    Update SortingAlgorithm.java

--- SortingAlgorithm.java
+++ SortingAlgorithm.java
@@ -31,4 +31,47 @@ public class SortingAlgorithm {
 
         return steps;
     }
+	public static List<SortStep> getSelectionSortSteps(int[] array) {
+	    List<SortStep> steps = new ArrayList<>();
+	    int[] arr = array.clone();
+	    int n = arr.length;
+
+	    for (int i = 0; i < n - 1; i++) {
+	        int minIndex = i;
+	        for (int j = i + 1; j < n; j++) {
+	            steps.add(new SortStep(arr.clone(), minIndex, j));
+	            if (arr[j] < arr[minIndex]) {
+	                minIndex = j;
+	            }
+	        }
+	        if (minIndex != i) {
+	            int temp = arr[i];
+	            arr[i] = arr[minIndex];
+	            arr[minIndex] = temp;
+	            steps.add(new SortStep(arr.clone(), i, minIndex));
+	        }
+	    }
+	    return steps;
+	}
+
+	public static List<SortStep> getInsertionSortSteps(int[] array) {
+	    List<SortStep> steps = new ArrayList<>();
+	    int[] arr = array.clone();
+	    int n = arr.length;
+
+	    for (int i = 1; i < n; i++) {
+	        int key = arr[i];
+	        int j = i - 1;
+
+	        while (j >= 0 && arr[j] > key) {
+	            arr[j + 1] = arr[j];
+	            j--;
+	            steps.add(new SortStep(arr.clone(), j + 1, j + 2));
+	        }
+	        arr[j + 1] = key;
+	        steps.add(new SortStep(arr.clone(), j + 1, i));
+	    }
+	    return steps;
+	}
 }
+
